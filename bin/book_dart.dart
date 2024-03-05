e47a08b105d29b3bf91c8a38dbb1b916c8b6d567
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2024-03-05 21:11:11 +0700

    initial commit

---
+++ bin/book_dart.dart
@@ -0,0 +1,12 @@
+import 'package:book_dart/book_dart.dart' as book_dart;
+
+// Define a class to represent a book
+class Book {
+}
+
+// Define a class to manage the book collection
+class BookCollection {
+}
+
+void main() {
+}
---
+++ test/book_dart_test.dart
@@ -0,0 +1,6 @@
+import 'package:book_dart/book_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // test();
+}
